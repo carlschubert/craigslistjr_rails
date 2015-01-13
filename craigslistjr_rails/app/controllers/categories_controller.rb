e40ae997d8b10b27e49ea249304d9da425d50312
Carl Schubert & Kevin Kwon <pair+carlschubert.kvnkwon@devbootcamp.com>
2015-01-13 15:46:18 -0500

    Passed category variable into category show view

--- craigslistjr_rails/app/controllers/categories_controller.rb
+++ craigslistjr_rails/app/controllers/categories_controller.rb
@@ -5,8 +5,8 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    category = Category.find(params[:id])
-    @articles = category.articles
+    @category = Category.find(params[:id])
+    @articles = @category.articles
   end
 
 end
