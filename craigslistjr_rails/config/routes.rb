001fb9ace19aec1129872b742ebda2af03af69da
Carl Schubert & Kevin Kwon <pair+carlschubert.kvnkwon@devbootcamp.com>
2015-01-13 12:26:12 -0500

    Created routes for articles

--- craigslistjr_rails/config/routes.rb
+++ craigslistjr_rails/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, :except => [:show, :index, :destroy]
   resources :sessions, :only => [:create]
   resources :categories, :only => [:show, :index]
+  resources :articles, :except => [:index]
   get '/login' => 'sessions#login'
   get '/logout' => 'sessions#logout'
 
