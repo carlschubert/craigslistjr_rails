0c48574cd60af6852c500721c83cc8d562ca0a59
Carl Schubert & Kevin Kwon <pair+carlschubert.kvnkwon@devbootcamp.com>
2015-01-13 12:37:37 -0500

    create seed file

--- craigslistjr_rails/db/seeds.rb
+++ craigslistjr_rails/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+categories = ['Casual Encounters', 'Human Organs', 'Motorcycles', 'Pets', 'Mysterious Candy']
+
+
+categories.each do |title|
+  Category.create(title: title)
+end
+
+
+
+52.times do
+  Article.create(title: Faker::Lorem.word, description: Faker::Lorem.sentence, price: rand(1..1000), email: Faker::Internet.email, category_id: rand(1..5), user_id: rand(1..2))
+end
+
+User.create(name: 'user', password: '123')
+User.create(name: 'peon', password: '123')
