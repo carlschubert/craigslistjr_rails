7cbf8cbc5c71433958ab69944030c69255c2e71e
Carl Schubert & Kevin Kwon <pair+carlschubert.kvnkwon@devbootcamp.com>
2015-01-13 11:16:37 -0500

    Set up session routes

--- craigslistjr_rails/config/routes.rb
+++ craigslistjr_rails/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :users, :except => [:show, :index, :destroy]
+  resources :sessions, :only => [:create]
+  get '/login' => 'sessions#login'
+  post '/logout' => 'sessions#logout'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
