44e6aa30c24d402885efb42a00cb56f3a1f7d6f7
Kevin <kvnkwon@gmail.com>
2015-01-14 17:46:37 -0500

    Add validation tests for article

--- craigslistjr_rails/spec/models/article_spec.rb
+++ craigslistjr_rails/spec/models/article_spec.rb
@@ -9,4 +9,22 @@ describe Article do
     it { should have_db_column(:email).of_type(:string) }
   end
 
+  describe "Validation Tests" do
+    let(:blanks){[nil, '']}
+
+    it { should have_valid(:title).when("Adorable Puppy needs home") }
+    it { should_not have_valid(:title).when(*blanks) }
+
+    it { should have_valid(:description).when("The puppy is just five months old! Needs home quick! Please email me!") }
+    it { should_not have_valid(:description).when(*blanks) }
+
+    it { should have_valid(:email).when("puppymaster@gmail.com") }
+    it { should_not have_valid(:email).when(*blanks) }
+
+    let(:invalid_amount){['99,999', -3, 0]}
+    it { should validate_numericality_of(:price) }
+    it { should have_valid(:price).when(1, 99999) }
+    it { should_not have_valid(:price).when(*invalid_amount) }
+  end
+
 end
