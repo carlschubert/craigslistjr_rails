47bf09befa313e6719abd1bf6a14a1081d437f93
Kevin <kvnkwon@gmail.com>
2015-01-14 18:22:02 -0500

    Create user sign in feature test with acceptance criteria

---
+++ craigslistjr_rails/spec/features/user_signs_in_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature "User signs in with an account", %q{
+  As a registered user,
+  I want to sign in,
+  so that I can access authenticated user features.
+} do
+
+# Acceptance Criteria:
+# * User must specify username and password.
+# * User is prompted with errors for invalid inputs.
+# * User is signed in upon correct input.
+
+
+end
