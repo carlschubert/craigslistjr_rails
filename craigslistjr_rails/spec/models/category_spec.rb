d7ec41c54ddceec4176654e5c3dcba58e82841ed
Kevin <kvnkwon@gmail.com>
2015-01-14 17:33:15 -0500

    Add validation tests for category

--- craigslistjr_rails/spec/models/category_spec.rb
+++ craigslistjr_rails/spec/models/category_spec.rb
@@ -6,4 +6,13 @@ describe Category do
     it { should have_db_column(:title).of_type(:string) }
   end
 
+  describe "Validation Tests" do
+    let(:blanks){[nil, '']}
+
+    it { should have_valid(:title).when("Casual Encounters") }
+    it { should_not have_valid(:title).when(*blanks) }
+
+    it { should validate_uniqueness_of(:title) }
+  end
+
 end
