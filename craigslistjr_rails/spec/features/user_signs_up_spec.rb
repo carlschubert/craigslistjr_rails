1e6dcb2b23cef7c5d0ed5265290790e824fcb042
Kevin <kvnkwon@gmail.com>
2015-01-14 17:53:53 -0500

    Create user sign up feature with acceptance criteria

---
+++ craigslistjr_rails/spec/features/user_signs_up_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature "User signs up", %q{
+  As a non-registered user,
+  I want to sign up,
+  so that I can access authenticated user features.
+} do
+
+# Acceptance Criteria:
+# * User must specify username and password.
+# * User must confirm password.
+# * User is prompted with errors for invalid inputs.
+# * User is signed in upon correct input.
+
+end
