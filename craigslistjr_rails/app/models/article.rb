1520759eb2cca59850c8b6a20a47a956595fb5fa
Kevin <kvnkwon@gmail.com>
2015-01-14 17:46:19 -0500

    Validate price for article to be a number greater than or equal to 1

--- craigslistjr_rails/app/models/article.rb
+++ craigslistjr_rails/app/models/article.rb
@@ -1,8 +1,9 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true
-  validates :price, presence: true
   validates :description, presence: true
   validates :email, presence: true
+
+  validates :price, :numericality => { :greater_than_or_equal_to => 1 }
 
   belongs_to :category, inverse_of: :articles
   belongs_to :user, inverse_of: :articles
