a7d577718f93348e3f37b5930c9293c1aa1ef3b1
Kevin <kvnkwon@gmail.com>
2015-01-14 18:04:25 -0500

    User signs up with valid input test

--- craigslistjr_rails/spec/features/user_signs_up_spec.rb
+++ craigslistjr_rails/spec/features/user_signs_up_spec.rb
@@ -12,4 +12,14 @@ feature "User signs up", %q{
 # * User is prompted with errors for invalid inputs.
 # * User is signed in upon correct input.
 
+  scenario 'with valid input' do
+    visit new_user_path
+    fill_in "User name", :with: "puppyoverlord"
+    fill_in "Password", :with: "puppy4ever"
+    fill_in "Password confirmation", :with: "puppy4ever"
+    click_button "Signup"
+
+    expect(page).to have_content 'Logout'
+  end
+
 end
