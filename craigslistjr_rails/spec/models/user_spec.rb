b1eed22b17a5d148e8e04a491711c5e9bb2eabb7
Kevin <kvnkwon@gmail.com>
2015-01-14 17:12:17 -0500

    Add validation tests for user model

--- craigslistjr_rails/spec/models/user_spec.rb
+++ craigslistjr_rails/spec/models/user_spec.rb
@@ -7,4 +7,14 @@ describe User do
     it { should have_db_column(:password_digest).of_type(:string) }
   end
 
+  describe "Validation Tests" do
+    let(:blanks){[nil, '']}
+
+    it { should have_valid(:user_name).when("masterkwon") }
+    it { should_not have_valid(:user_name).when(*blanks) }
+
+    it { should have_valid(:password).when("iluvu") }
+    it { should_not have_valid(:password).when(*blanks) }
+  end
+
 end
